a1994e0ee8e86c52b2975ff1f41a14be0ed099d4
Forrest Lin <forrest.lin.developer@gmail.com>
2023-04-15 23:13:00 +1000

    init client

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import { Chat, Login } from "@/components";
+
+const App = () => {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
